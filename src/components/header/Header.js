6f620e38fec5818d27efb36d419b535914a3cc18
Raul Oropesa <alexoropesa1982@yahoo.com>
2020-12-28 03:44:49 -0800

    First Commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,6 @@
+import "./Header.css";
+
+function Header() {
+  return <section className="header"></section>;
+}
+export default Header;
---
+++ src/components/header/title/Title.js
@@ -0,0 +1,10 @@
+import "./Title.css";
+
+function Title() {
+  return (
+    <div className="title">
+      <h1>Raul Oropesa</h1>
+    </div>
+  );
+}
+export default Title;
