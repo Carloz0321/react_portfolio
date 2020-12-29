ddf459870b03ea0d7a3d9f1cc03b06ba327af6dd
Raul Oropesa <alexoropesa1982@yahoo.com>
2020-12-29 15:16:03 -0800

    added logo to Contact.js

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,9 +1,16 @@
+import "./LI-Logo.png";
 import "./Contact.css";
 
 function Contact() {
   return (
     <ul>
-      <li></li>
+      <li>
+        <a
+          className="linkedInLogo"
+          href="https://www.linkedin.com/in/raojr12lr21af17rf17rioiii21/"
+          alt="linkedin"
+        ></a>
+      </li>
       <li></li>
       <li></li>
     </ul>
