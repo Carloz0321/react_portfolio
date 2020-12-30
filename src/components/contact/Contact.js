3f65a3ccb122a37204b8d4b5f1308a848f1a1940
Raul Oropesa <alexoropesa1982@yahoo.com>
2020-12-29 17:40:29 -0800

    added logos to Contact.js, added acitveStyle to links, and updated css

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -3,17 +3,26 @@ import "./Contact.css";
 
 function Contact() {
   return (
-    <ul>
-      <li>
-        <a
-          className="linkedInLogo"
-          href="https://www.linkedin.com/in/raojr12lr21af17rf17rioiii21/"
-          alt="linkedin"
-        ></a>
-      </li>
-      <li></li>
-      <li></li>
-    </ul>
+    <div className="container">
+      <h4>Check me out on LinkedIn or GitHub just click on one below.</h4>
+      <h5>Or contact me via email: alexoropesa1982@yahoo</h5>
+      <ul className="logoLinks">
+        <li>
+          <a
+            className="linkedInLogo"
+            href="https://www.linkedin.com/in/raojr12lr21af17rf17rioiii21/"
+            alt="linkedin"
+          ></a>
+        </li>
+        <li>
+          <a
+            className="gitHub"
+            href="https://github.com/Rauloropesa3"
+            alt="gitHub"
+          ></a>
+        </li>
+      </ul>
+    </div>
   );
 }
 
--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -7,16 +7,24 @@ function Navbar() {
       <h6>Raul Oropesa</h6>
       <ul className="links">
         <li className="home">
-          <NavLink to="/">Home</NavLink>
+          <NavLink to="/" exact activeStyle={{ color: "red" }}>
+            Home
+          </NavLink>
         </li>
         <li className="about">
-          <NavLink to="/about">About Me</NavLink>
+          <NavLink to="/about" exact activeStyle={{ color: "red" }}>
+            About Me
+          </NavLink>
         </li>
         <li className="port">
-          <NavLink to="/port">Portfolio</NavLink>
+          <NavLink to="/port" exact activeStyle={{ color: "red" }}>
+            Portfolio
+          </NavLink>
         </li>
         <li className="cont">
-          <NavLink to="/contact">Contact</NavLink>
+          <NavLink to="/contact" exact activeStyle={{ color: "red" }}>
+            Contact
+          </NavLink>
         </li>
       </ul>
     </div>
